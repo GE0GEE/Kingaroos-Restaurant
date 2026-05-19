9a1438791910a4f0cd8af72cb6cd55635b353d44
GE0GEE <91040790+GE0GEE@users.noreply.github.com>
2026-05-19 19:07:48 +0800

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Header } from "./Header";
-import { Footer } from "./Footer";
+import { Footer } from "../pages/Footer";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -7,9 +7,6 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    // THE FIX IS HERE: The `bg-cream-50` class has been removed.
-    // This allows the main layout to be "transparent", letting the background
-    // color set on the <body> tag (which changes with the theme) show through.
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-1">{children}</main>
